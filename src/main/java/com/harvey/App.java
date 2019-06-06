363b6927af48e9746f96ac2367233ea3e96ff4dd
hlawton <hlawton@hotmail.com>
2019-06-06 10:52:34 -0600

    new interface

--- src/main/java/com/harvey/App.java
+++ src/main/java/com/harvey/App.java
@@ -1,38 +1,47 @@
 package com.harvey;
 
+import java.awt.*;
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import java.io.FileInputStream;
 
 public class App extends Application {
 
     @Override
-    public void init() throws Exception {
-        super.init();
-        // System.out.println("Inside init() method! Perform necessary initializations here.");
-    }
-
-    @Override
     public void start(Stage primaryStage) throws Exception {
-        Label label = new Label("... no banks ...");
-        label.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(label, 500, 350);
+        primaryStage.setTitle("Ricoh ...");
 
-        primaryStage.setTitle("Bitcoin ...");
+        FileInputStream input = new FileInputStream("C:\\Users\\Harvey\\Desktop\\Java\\bitcoin-app\\src\\main\\java\\com\\harvey\\Ricoh-Logo.jpg");
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+
+        Button button = new Button("", imageView);
+
+        // action event 
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) 
+            { 
+                Toolkit.getDefaultToolkit().beep();
+            } 
+        }; 
+  
+        // when button is pressed 
+        button.setOnAction(event); 
+
+        Scene scene = new Scene(button, 300, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
 
-    @Override
-    public void stop() throws Exception {
-        super.stop();
-        // System.out.println("Inside stop() method! Destroy resources. Perform Cleanup.");
     }
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
-
 }
