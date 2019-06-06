115c218591632a84d1ff55a69cfed47fea8d979c
hlawton <hlawton@hotmail.com>
2019-06-05 18:49:30 -0600

    new interface

--- src/main/java/com/harvey/App.java
+++ src/main/java/com/harvey/App.java
@@ -1,13 +1,40 @@
 package com.harvey;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        System.out.println("Inside init() method! Perform necessary initializations here.");
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Label label = new Label("Bitcoin app");
+        label.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(label, 500, 350);
+
+        primaryStage.setTitle("... no banks ...");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        System.out.println("Inside stop() method! Destroy resources. Perform Cleanup.");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+        // System.out.println( "Hello World!" );
+    }
+
 }
